2c79ee5b4b053e97caa94d70c5382627741e90b2
Stjepan1999 <106433830+Stjepan1999@users.noreply.github.com>
2024-01-17 19:09:04 +0100

    Create input containers for personal details

--- src/components/InputContainer.jsx
+++ src/components/InputContainer.jsx
@@ -3,11 +3,21 @@ import '../style.css';
 export function InputContainer({ id, label, type, value, placeholder, onChange }) {
   return (
     <div className="input-container">
-      <label htmlFor={id}>
-        {' '}
+      <label htmlFor={id} className="label-text">
         {label}:
-        <input type={type} id={id} value={value} placeholder={placeholder} onChange={onChange} />
       </label>
+      {type === 'textarea' ? (
+        <textarea
+          className="textarea"
+          type={type}
+          id={id}
+          value={value}
+          placeholder={placeholder}
+          onChange={onChange}
+        ></textarea>
+      ) : (
+        <input className="input" type={type} id={id} value={value} placeholder={placeholder} onChange={onChange} />
+      )}
     </div>
   );
 }
--- src/components/PersonalDetails.jsx
+++ src/components/PersonalDetails.jsx
@@ -3,19 +3,43 @@ import { InputContainer } from './InputContainer';
 import '../style.css';
 
 export function PersonalDetails() {
-  const value = 'Hi';
+  const value = 'John';
   const handleChange = () => {
     console.log('handling change');
   };
   return (
     <div className="personal-details">
       <h1>Personal Details</h1>
-      <form>
+      <form className="form-container">
         <InputContainer
           label="First Name"
           type="text"
           id="first-name"
           placeholder="Enter First Name"
+          value={value}
+          onChange={handleChange}
+        />
+        <InputContainer
+          label="Last Name"
+          type="text"
+          id="last-name"
+          placeholder="Enter Last Name"
+          value={value}
+          onChange={handleChange}
+        />
+        <InputContainer
+          label="Professional Title"
+          type="text"
+          id="professional-title"
+          placeholder="Software Developer"
+          value={value}
+          onChange={handleChange}
+        />
+        <InputContainer
+          label="Summary"
+          type="textarea"
+          id="summary"
+          placeholder="Write summary about yourself"
           value={value}
           onChange={handleChange}
         />

917bcfd0d8e1b2bcdbba3fbf7038c8b581ccbf0c
Stjepan1999 <106433830+Stjepan1999@users.noreply.github.com>
2024-01-17 20:51:20 +0100

    Create resume info section

--- src/components/ResumeContainer.jsx
+++ src/components/ResumeContainer.jsx
@@ -1,5 +1,10 @@
+import { ResumeInfoSection } from './ResumeInfo';
 import '../style.css';
 
 export function ResumeContainer() {
-  return <div className="resume"></div>;
+  return (
+    <div className="resume-container">
+      <ResumeInfoSection />
+    </div>
+  );
 }
---
+++ src/components/ResumeInfo.jsx
@@ -0,0 +1,16 @@
+export function ResumeInfoSection() {
+  return (
+    <div className="resume-info-section">
+      <div className="resume-name-section">
+        <div className="resume-name">John Smith</div>
+        <div className="resume-title">Web Developer</div>
+      </div>
+      <div className="resume-contact-section">
+        <div className="resume-email">john.smith@gmail.com</div>
+        <div className="resume-phone">+44 8327 0423 743</div>
+        <div className="resume-address">London, UK</div>
+        <div className="resume-website">www.linkedin.com</div>
+      </div>
+    </div>
+  );
+}
